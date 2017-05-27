7015cc198e1745ff92bca420b5f9eb47092c37b7
Ravi Akella <ravisankar.akella@gmail.com>
2017-05-27 07:08:38 -0500

    Initial commit to read the permissions from a csv file

---
+++ util/permissions_reader.py
@@ -0,0 +1,12 @@
+import unicodecsv
+import os
+
+def read_permissions_csv_file(num_permissions):
+    permissions_list = []
+
+    script_dir = os.path.dirname(__file__)
+    full_path = os.path.join(script_dir, '../properties-files/permission-values.csv')
+    with open(full_path, 'rb') as f:
+        reader = unicodecsv.DictReader(f)
+        permissions_list = reader.fieldnames
+    return permissions_list[:num_permissions]
